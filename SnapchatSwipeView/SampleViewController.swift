5f369cdf6ab7f523bd66d253daa6231167420c2b
Santiago Gutierrez <ssgutierrez42@gmail.com>
2017-11-04 22:53:15 -0700

    Simplified Swipping to only allow for left, middle, and right. Included a way to swipe between screens using buttons as triggers.

---
+++ SnapchatSwipeView/SampleViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SampleViewController.swift
+//  SnapchatSwipeView
+//
+//  Created by Santiago Gutierrez on 11/4/17.
+//  Copyright © 2017 Brendan Lee. All rights reserved.
+//
+
+import Foundation
---
+++ SnapchatSwipeView/SnapBaseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SnapBaseViewController.swift
+//  SnapchatSwipeView
+//
+//  Created by Santiago Gutierrez on 11/4/17.
+//  Copyright © 2017 Brendan Lee. All rights reserved.
+//
+
+import Foundation
